c995e142595e4de6a2cb0566615d3ce49993495b
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-12 14:25:52 -0700

    articles on page and working

--- Article/Article.js
+++ Article/Article.js
@@ -125,6 +125,13 @@ function news (title, date, firstParagraph, secondParagraph, thirdParagraph) {
   artdate.classList.add('date');
   expand.classList.add('expandButton');
 
+  arttitle.textContent = title;
+  artdate.textContent = date;
+  par1.textContent = firstParagraph;
+  par2.textContent = secondParagraph;
+  par3.textContent = thirdParagraph;
+  expand.textContent = 'expand';
+
   article.addEventListener('click', event => {
     article.classList.toggle('article-open');
   });
@@ -138,7 +145,16 @@ function news (title, date, firstParagraph, secondParagraph, thirdParagraph) {
   Step 3: return the entire component.
 
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+*/
 
+const articles = document.querySelector('.articles');
+   
+data.map(data => {
+  articles.appendChild(news(data.title, data.date, data.firstParagraph, data.secondParagraph, data.thirdParagraph));
+});
+
+/*
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
